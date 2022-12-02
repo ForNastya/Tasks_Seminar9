8b4c04a07615408fc2a1be7b35af87ade079807e
Anastasyia <moya_pochta_n.k@mail.ru>
2022-12-02 11:13:58 +0700

    Task_05

---
+++ Task_05/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+string ProgramN(int numberA, int numberB, int count)
+{
+    if(count!=0)
+    {
+        --count;
+        int sum = numberA + numberB;
+        return $"{sum} " + ProgramN(numberB, sum, count);
+    }
+    else
+    return String.Empty;
+}
+
+Console.WriteLine("Enter a: ");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter b: ");
+int numberB = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter n: ");
+int count = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"{numberA} " + $"{numberB} " + ProgramN(numberA, numberB, count));
