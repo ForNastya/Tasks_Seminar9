ab7ed241ab1c2599161ad9f882b345233b1edb39
Anastasyia <moya_pochta_n.k@mail.ru>
2022-12-02 09:43:09 +0700

    Task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int Sum(int n, int m)
+{
+    if(n == m) 
+    return n;
+    else 
+    return n + Sum(n + 1, m);
+}
+Console.WriteLine(Sum(1, 10));
