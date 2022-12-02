4e23ad90fc77fce434b6223d30949d2487180b77
Anastasyia <moya_pochta_n.k@mail.ru>
2022-12-02 09:52:12 +0700

    Task_01

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Показать натуральные числа от M до N, N и M заданы
+string Numbers (int n, int m)
+{
+    if (n <= m)
+    return Numbers(n + 1, m) + $"{n}";
+    else
+    {
+        return String.Empty;
+    }
+}
+Console.WriteLine(Numbers(1, 10));
