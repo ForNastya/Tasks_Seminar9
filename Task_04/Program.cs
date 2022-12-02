c88d52b6386015edb4d0aaa4143f935f761fceff
Anastasyia <moya_pochta_n.k@mail.ru>
2022-12-02 11:02:21 +0700

    Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую степень B
+int Exponentiation (int a, int b)
+{
+    if (b == 0) 
+    return 1;
+    else
+    return a * Exponentiation(a, b - 1);
+}
+Console.WriteLine("Enter a: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Enter b: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Number {a} to the extent {b} = {Exponentiation(a,b)}");
